f19c6bf0658e739045aeed224c33367241611581
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-12-30 23:47:35 +0600

    Uploaded

---
+++ js/script.js
@@ -0,0 +1,116 @@
+// =======================================================
+// -- Home Slider Functionality
+// =======================================================
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  // grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    // dynamicBullets: true,
+    // renderBullet: function (index, className) {
+    //   return '<span class="' + className + '">' + (index + 1) + "</span>";
+    // },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// =======================================================
+// -- Nav Menu Functionality
+// =======================================================
+
+// Nav open close
+const body = document.querySelector('body'),
+      navMenu = body.querySelector('.menu-content'),
+      navOpenBtn = body.querySelector('.navOpen-btn'),
+      navCloseBtn = navMenu.querySelector('.navClose-btn');
+
+if(navMenu && navOpenBtn){
+  navOpenBtn.addEventListener("click", () =>{
+    navMenu.classList.add("open");
+    body.style.overflowY = "hidden";
+  })
+}
+
+if(navMenu && navCloseBtn){
+  navCloseBtn.addEventListener("click", () =>{
+    navMenu.classList.remove("open");
+    body.style.overflowY = "scroll";
+  })
+}
+
+// Change header bg color
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  if(scrollY > 5){
+    document.querySelector("header").classList.add("header-active");
+  }else{
+    document.querySelector("header").classList.remove("header-active");
+  }
+
+  // Scroll up button
+  const scrollUpBtn = document.querySelector('.scrollUp-btn');
+
+  if(scrollY > 250){
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  }else{
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
+  
+  
+  // Nav link indicator
+
+  const sections = document.querySelectorAll('section[id]');
+  sections.forEach(section =>{
+    const sectionHeight = section.offsetHeight,
+          sectionTop = section.offsetTop - 100;
+
+          let navId = document.querySelector(`.menu-content a[href='#${section.id}']`);
+          if(scrollY > sectionTop && scrollY <=  sectionTop + sectionHeight){
+            navId.classList.add("active-navlink");           
+          }else{
+            navId.classList.remove("active-navlink");     
+          }
+          
+          navId.addEventListener("click", () => {
+            navMenu.classList.remove("open");
+            body.style.overflowY = "scroll";
+          })
+  })
+})
+
+
+// =======================================================
+// -- Theme Button Functionality
+// =======================================================
+
+const themeBtn = document.querySelectorAll(".theme-btn");
+
+for(let i = 0; i < themeBtn.length; i++) {
+    themeBtn[i].addEventListener('click', () => {
+      document.body.classList.toggle('light-theme');
+      document.body.classList.toggle('dark-theme');
+
+      for(let i = 0; i < themeBtn.length; i++) {
+          themeBtn[i].classList.toggle("light");
+          themeBtn[i].classList.toggle("dark");
+          // localStorage.setItem("theme-btn", themeBtn[i].classList.value.slice(27));
+      }
+
+      // localStorage.setItem("theme-name", document.body.classList.value);
+    })
+}
+
+
+// document.body.classList.add(localStorage.getItem("theme-name"));
